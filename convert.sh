da6ebef9c01a0c0e83bf48ce01e94c289e2d70b3
Will Wheeler <willfulbard@gmail.com>
2023-04-10 15:29:14 -0700

    Add first music to process

---
+++ convert.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd music
+
+cat << EOF > ../build/index.html
+<h1>Will's Music Files</h1>
+
+<ul>
+
+EOF
+
+for f in *.abc
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  filename="${f%.*}"
+  abcm2ps "$f"
+  ps2pdf Out.ps "$filename.pdf"
+  mv "$filename.pdf" ../build/
+  echo "<li><a href=\"$filename.pdf\">$filename.pdf</a></li> " >> ../build/index.html
+done
+
+cat << EOF >> ../build/index.html
+
+</ul>
+
+EOF
